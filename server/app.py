d75fbb101ea52c6e99d2f3b9793e667981f80377
how <h.melnyczuk@gmail.com>
2019-08-15 10:44:44 +0200

    vvvvvvvv

--- server/app.py
+++ server/app.py
@@ -54,4 +54,5 @@ def chat():
 if __name__ == ("__main__"):
     port = int(os.environ.get("PORT", 8080))
     print("@@@@@@@@@@@@@@@ PORT:", port)
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=port)
+
